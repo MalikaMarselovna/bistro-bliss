a481bc30c2725f255465eec53ba01fcbea7ce247
MalikaMarselovna <mmukashova1@gmail.com>
2023-11-22 13:49:31 +0600

    add js

--- js/core.js
+++ js/core.js
@@ -1,1 +1,7 @@
-document.querySelector ('.js-btn').addEventListener('click', function (){document.querySelector('.js-btn').classList.add('active')})
+document.querySelector ('.js-btn').addEventListener('click', function (){document.querySelector('.js-btn').classList.add('active')
+document.querySelector('.js-btn').computedStyleMap.height = "200px"
+document.querySelector('.js-btn').textContent.Content = "change a button's text"
+
+var newhr = document.createElement('<p> paragraph</p>');
+document.querySelector('.js-btn').insertAdgacenElement(aftenoon)
+})
